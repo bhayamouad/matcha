6948274b679c34d8ff44af9d99baa188832abb9c
mbhaya <mouadbhaya@gmail.com>
2020-11-21 17:16:00 +0100

    added the msql connection

--- web/app.js
+++ web/app.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'db',
+  user: 'tirach',
+  password: 'rach',
+  database: 'test'
+})
 
+connection.connect()
 const app = express();
+connection.query('SELECT * FROM hello', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('The solution is: ', rows)
+})
+
+connection.end()
+
 
 app.use(bodyParser.json());
 app.get('/',(req,res,next) => {
-    res.send("hello worldssssssssss");
+    res.send("hello worldsssss");
 })
 app.listen(3000);
