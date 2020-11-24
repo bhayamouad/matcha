025c41ecf7de76a8c66ea042491eda0025f0e0da
mbhaya <mouadbhaya@gmail.com>
2020-11-24 12:11:54 +0100

    added more tables to the db schema

--- db/init-development.sql
+++ db/init-development.sql
@@ -50,10 +50,67 @@ ALTER TABLE `tags` MODIFY `id_tag` int(11) NOT NULL AUTO_INCREMENT;
 
 CREATE TABLE `users_tags`(
                             `user_id` int(11) NOT NULL,
-                            `tag_id` int(11) NOT NULL
+                            `tag_id` int(11) NOT NULL,
+                            `created_at` TIMESTAMP NOT NULL DEFAULT NOW(),
+                            `updated_at` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW()
                         );
 ALTER TABLE `users_tags` ADD PRIMARY KEY (`user_id`,`tag_id`);
 ALTER TABLE `users_tags` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE `users_tags` ADD FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id_tag`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+CREATE TABLE `positions` (
+                            `id_position` int(11) NOT NULL,
+                            `city` varchar(20),
+                            `lat` FLOAT( 10, 6 ),
+                            `lng` FLOAT( 10, 6 ),
+                            `user_id` int(11) NOT NULL,
+                            `created_at` TIMESTAMP NOT NULL DEFAULT NOW(),
+                            `updated_at` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW()
+                        );
+ALTER TABLE `positions` ADD PRIMARY KEY (`id_position`);
+ALTER TABLE `positions` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+CREATE TABLE `matchs` (
+                        `id_match` int(11) NOT NULL,
+                        `first_profile` int(11) NOT NULL,
+                        `second_profile` int(11) NOT NULL,
+                        `created_at` TIMESTAMP NOT NULL DEFAULT NOW(),
+                        `updated_at` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW()
+                    );
+
+ALTER TABLE `matchs` ADD PRIMARY KEY (`id_match`);
+ALTER TABLE `matchs` ADD FOREIGN KEY (`first_profile`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `matchs` ADD FOREIGN KEY (`second_profile`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+CREATE TABLE `messages` (
+                            `id_message` int(11) NOT NULL,
+                            `message` varchar(255) NOT NULL,
+                            `sender_id` int(11),
+                            `receiver_id` int(11),
+                            `created_at` TIMESTAMP NOT NULL DEFAULT NOW()
+                    );
+
+ALTER TABLE `messages` ADD PRIMARY KEY (`id_message`);
+ALTER TABLE `messages` ADD FOREIGN KEY (`sender_id`) REFERENCES `users` (`id_user`) ON DELETE SET NULL;
+ALTER TABLE `messages` ADD FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id_user`) ON DELETE SET NULL;
+
+CREATE TABLE `reports` (
+                            `id_report` int(11) NOT NULL,
+                            `reason` varchar(50) NOT NULL,
+                            `reporter_id` int(11) NOT NULL,
+                            `reported_id` int(11) NOT NULL,
+                            `created_at` TIMESTAMP NOT NULL DEFAULT NOW()
+                    );
+ALTER TABLE `reports` ADD PRIMARY KEY (`id_report`);
+ALTER TABLE `reports` ADD FOREIGN KEY (`reporter_id`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `reports` ADD FOREIGN KEY (`reported_id`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+CREATE TABLE `blocks` (
+                            `id_block` int(11) NOT NULL,
+                            `blocker_id` int(11) NOT NULL,
+                            `blocked_id` int(11) NOT NULL,
+                            `created_at` TIMESTAMP NOT NULL DEFAULT NOW()
+                    );
+ALTER TABLE `blocks` ADD PRIMARY KEY (`id_block`);
+ALTER TABLE `blocks` ADD FOREIGN KEY (`blocker_id`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `blocks` ADD FOREIGN KEY (`blocked_id`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
