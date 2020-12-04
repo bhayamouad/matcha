8518be3e568f9617aedd661738b19615aa8816ca
mbhaya <mouadbhaya@gmail.com>
2020-12-04 22:45:18 +0100

    added the vuejs client for the frontend

---
+++ client/src/components/Register.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input
+      type="text"
+      name="username"
+      placeholder="username"
+      v-model="username"
+    />
+    <input type="email" name="email" placeholder="email" v-model="email" />
+    <button @click="register">Register</button>
+  </div>
+</template>
+<script>
+import Authentication from '@/services/Authentication'
+export default {
+  data () {
+    return {
+      username: '',
+      email: ''
+    }
+  },
+  methods: {
+    register () {
+      const response = Authentication.register({
+        username: this.username,
+        email: this.email
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+<style>
+</style>
--- web/app.js
+++ web/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors =require('cors')
 
 const app = express()
-app.use(bodyParser.json());
-app.get('/',(req,res,next) => {
-    res.send("hello world");
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/',(req,res) => {
+    res.send({message:"hello world"});
 })
+
+app.post('/register',(req,res) =>{
+    res.send({message:`hello ${req.body.username} your account was created`})
+})
+
 app.listen(3000);
