132875413fceb527bc52b937747ecd86f84afe75
benanianas <benanianas21@gmail.com>
2020-12-11 16:45:07 +0100

    merge to master

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
     port: 8080, // default: 3000
     host: '0.0.0.0' // default: localhost
   },
-
+  loading: false,
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'client',
