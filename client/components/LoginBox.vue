1d19ecdbc95a22dfa49317c5082b5e00f87b4169
benanianas <benanianas21@gmail.com>
2020-12-14 14:08:12 +0100

    fixed login box bugs

--- client/components/LoginBox.vue
+++ client/components/LoginBox.vue
@@ -2,17 +2,23 @@
   <div class="the-box">
     <div id="blogo-container"><img id="blogo" src="~/assets/blogo.png" /></div>
     <div id="login-form">
-      <b-field label="Username or Email">
+      <b-field label="Username or Email"
+      v-bind:type="{'is-danger': errors.email}"
+      v-bind:message="errors.email"
+      >
         <b-input v-model="user.email" placeholder="e.g. tirachrach@gmail.com"></b-input>
       </b-field>
-      <b-field label="Password">
+      <b-field label="Password"
+      v-bind:type="{'is-danger': errors.password}"
+      v-bind:message="errors.password"
+      >
         <b-input
           type="password"
           v-model="user.password"
           placeholder="********"
         ></b-input>
       </b-field>
-        <div id="f-pswd"><NuxtLink to="/register">Forgot password ? </NuxtLink></div>
+        <div id="f-pswd"><NuxtLink to="/reset">Forgot password ? </NuxtLink></div>
       <b-button @click="login" type="is-primary" expanded>Login</b-button>
 
     </div>
@@ -23,6 +29,14 @@
 </template>
 
 <script>
+const validateEmail = (email) => {
+  if(!email) return {valid: false, error: 'Please fill out this field.'}
+  return { valid: true, error: null };
+}
+const validatePassword = (pwd) => {
+  if(!pwd) return {valid: false, error: 'Please fill out this field.'}
+  return { valid: true, error: null };
+}
 
 export default {
     data(){
@@ -31,13 +45,32 @@ export default {
                 email: '',
                 password: ''
                 },
+            valid: null,
+            errors: {}
             }
     },
     methods:{
         async login(){
+          this.valid = true
+          const validemail = validateEmail(this.user.email)
+          this.errors.email = validemail.error
+          if (this.valid) this.valid = validemail.valid
+
+          const validpwd = validatePassword(this.user.password)
+          this.errors.password = validpwd.error
+          if (this.valid) this.valid = validpwd.valid
+
+          if(this.valid)
+          {
+            console.log('hello');
             const res = await this.$axios.$post('/account/login', this.user)
             if(res.message)
                 this.loginError(res.message);
+          }
+          this.user = {
+            email: this.user.email,
+            password: this.user.password
+          }
         },
         loginError(msg) {
             this.$buefy.toast.open({
