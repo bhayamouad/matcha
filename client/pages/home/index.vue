117aad1386396d6ec324af9eb2722e2f894fccba
benanianas <benanianas21@gmail.com>
2020-12-28 18:12:18 +0100

    merge /home index

--- client/pages/home/index.vue
+++ client/pages/home/index.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <section>
     <b-steps type="is-success" v-model="active">
       <b-step-item label="Account" icon="account-key"></b-step-item>
@@ -18,17 +17,7 @@ export default {
   components: {
     SetProfile
   },
-=======
-  <div>hello from home {{theret}}  </div>
-</template>
-
-<script>
-// import Cookies from 'js-cookie'
-// import { mapMutations } from 'vuex'
-
-export default {
   middleware: 'authorization',
->>>>>>> anas
   name: "HomePage",
   head() {
     return {
