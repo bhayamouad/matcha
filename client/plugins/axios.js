87c0c0b7dc0505c64b407936a4b7ed0811c32187
benanianas <benanianas21@gmail.com>
2020-12-30 19:09:32 +0100

    plugin comment

--- client/plugins/axios.js
+++ client/plugins/axios.js
@@ -6,6 +6,7 @@ export default function ({ $axios, store}) {
         //  hi mouad to test this  plugin uncomment the last line 
         // and change accTokenExp in the api to 10 seconds 
         // refresh the ip:8080/home multiple times and watch the console
+        // ps : to be able to use the header we need to expose the header check app.js in the Api
         
         //console.log(res.headers.acctok)
     })
