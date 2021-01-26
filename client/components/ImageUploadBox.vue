b5497b508737d6b7e80482059f7bf944d245db95
mbhaya <mouadbhaya@gmail.com>
2021-01-26 10:34:22 +0100

    fixed image crop on upload

--- client/components/ImageUploadBox.vue
+++ client/components/ImageUploadBox.vue
@@ -1,50 +1,94 @@
 <template>
-  <draggable
-    v-model="upoloadImages"
-    v-bind="dragOptions"
-    @start="drag = true"
-    @end="drag = false"
-  >
-    <transition-group type="transition" class="columns is-multiline" :name="!drag ? 'flip-list' : null">
-      <div class="column is-4 " :class="{'draggable':image.url}" v-for="(image, index)  in upoloadImages" :key="image.position">
-        <div class="card" aria-id="contentIdForA11y3" >
-          <div class="card-content">
-            <div class="content">
-              <input
-                type="file"
-                style="display: none"
-                multiple
-                ref="upload"
-                accept="image/*"
-                @change="onFilePicked($event,index)"
-              />
+  <div>
+    <draggable v-model="uploadImages" v-bind="dragOptions" @start="drag = true" @end="drag = false">
+      <transition-group
+        type="transition"
+        class="columns is-multiline"
+        :name="!drag ? 'flip-list' : null"
+      >
+        <div
+          class="column is-4"
+          :class="{'draggable':image.url}"
+          v-for="(image, index)  in uploadImages"
+          :key="image.position"
+        >
+          <div class="card" aria-id="contentIdForA11y3">
+            <div class="card-content">
+              <div class="content">
+                <input
+                  type="file"
+                  style="display: none"
+                  multiple
+                  ref="upload"
+                  accept="image/*"
+                  @change="onFilePicked($event,index)"
+                />
 
-              <span v-if="image.url" class="material-icons del-icons" @click="deleteImage(index)">clear</span>
-              <span v-else class="material-icons up-icons" @click="uploadClick(index)">add_circle_outline</span>
-              <img :src="image.url" alt />
+                <span
+                  v-if="image.url"
+                  class="material-icons del-icons"
+                  @click="deleteImage(index)"
+                >clear</span>
+                <span
+                  v-else
+                  class="material-icons up-icons"
+                  @click="uploadClick(index)"
+                >add_circle_outline</span>
+                <img v-if="image.url" :src="image.url" :width="280" :height="380"/>
+              </div>
             </div>
           </div>
         </div>
+      </transition-group>
+    </draggable>
+    <b-modal v-model="isImageModalActive" :can-cancel="['x', 'escape']">
+      <div>
+        <cropper
+          ref="cropper"
+          v-if="uploadImages[openModel]"
+          :src="uploadImages[openModel].url"
+          :auto-zoom="true"
+          :stencil-props="{
+		        aspectRatio: 2/3,
+	        }"
+          
+        />
+        <div class="button-wrapper">
+          <span class="button" @click="crop(openModel)">Add Photo</span>
+        </div>
       </div>
-    </transition-group>
-  </draggable>
+    </b-modal>
+  </div>
 </template>
 
 <script>
 import draggable from "vuedraggable";
+import { Cropper } from "vue-advanced-cropper";
+import "vue-advanced-cropper/dist/style.css";
+
+function srcToFile(src, fileName, mimeType){
+    return (fetch(src)
+        .then(function(res){return res.arrayBuffer();})
+        .then(function(buf){return new File([buf], fileName, {type:mimeType});})
+    );
+}
+
 let images = [null, null, null, null, null];
-const formData = new FormData()
+const formData = new FormData();
 export default {
   display: "Transitions",
   components: {
+    Cropper,
     draggable
   },
   data() {
     return {
-      upoloadImages: images.map((url, index) => {
+      uploadImages: images.map((url, index) => {
         return { url, position: index, file: null };
       }),
-      drag: false
+      drag: false,
+      isImageModalActive: false,
+      openModel: null
     };
   },
   computed: {
@@ -54,58 +98,73 @@ export default {
         group: "description",
         disabled: false,
         ghostClass: "ghost",
-        draggable: '.draggable'
+        draggable: ".draggable"
       };
     }
   },
   methods: {
     uploadClick(index) {
-      if(index === 0)
-      {
+      if (index === 0) {
         let fileInputElement = this.$refs.upload[index];
         fileInputElement.click();
-        return
-        
+        return;
       }
-      if ((index > 0 && this.upoloadImages[index-1].url) || this.upoloadImages[index].url){ 
+      if (
+        (index > 0 && this.uploadImages[index - 1].url) ||
+        this.uploadImages[index].url
+      ) {
         let fileInputElement = this.$refs.upload[index];
         fileInputElement.click();
-        return
+        return;
       }
     },
-    deleteImage(index){
-       this.$delete(this.upoloadImages, index)
-       this.upoloadImages.forEach((image,i) => {
-         image.position = i
-       })
-      this.upoloadImages.push({url:null,position:4,file:null})
+    deleteImage(index) {
+      this.$delete(this.uploadImages, index);
+      this.uploadImages.forEach((image, i) => {
+        image.position = i;
+      });
+      this.uploadImages.push({ url: null, position: 4, file: null });
     },
-    onFilePicked(event,index) {
+    onFilePicked(event, index) {
       const files = event.target.files;
       if (files.length) {
         const fileReader = new FileReader();
-        fileReader.addEventListener("load", (e) => {
-          this.imageUrl = e.target.result;
-          this.upoloadImages[index].url = this.imageUrl;
-          this.upoloadImages[index].file = event.target.files[0]
+        fileReader.addEventListener("load", e => {
+          this.uploadImages[index].url = e.target.result;
+          this.uploadImages[index].file = event.target.files[0];
+          this.isImageModalActive = true;
+          this.openModel = index;
         });
         fileReader.readAsDataURL(event.target.files[0]);
+      } else {
+        this.uploadImages[index].url = null;
+        this.uploadImages[index].file = null;
       }
-      else {
-        this.upoloadImages[index].url = null;
-        this.upoloadImages[index].file = null;
-        } 
     },
-    async saveImages(){
-        this.upoloadImages.forEach((image,index) => formData.append("images", image.file))
-        const res = await this.$axios.$post("/account/saveImages", formData ,{headers: {'content-Type': 'multipart/form-data' } })
-        formData.delete("images");
-        if(res.error)
-          return false
-      return true
+    async saveImages() {
+      this.uploadImages.forEach((image, index) =>
+        formData.append("images", image.file)
+      );
+      const res = await this.$axios.$post("/account/saveImages", formData, {
+        headers: { "content-Type": "multipart/form-data" }
+      });
+      formData.delete("images");
+      if (res.error) return false;
+      return true;
+    },
+    crop(index) {
+      const { canvas } = this.$refs.cropper.getResult();
+      this.uploadImages[index].url = canvas.toDataURL();
+      this.isImageModalActive = false
+      const mimeType = this.uploadImages[index].url.split(':')[1]
+      const ext = mimeType.split(';')[0]
+      srcToFile(
+        this.uploadImages[index].url,
+        this.uploadImages[index].file.name,
+        ext
+      ).then( (file) => this.uploadImages[index].file = file)
     }
-    
-  } 
+  }
 };
 </script>
 
@@ -114,5 +173,11 @@ export default {
   background-color: transparent;
   padding: 1.5rem;
   height: 370px;
+}
+
+.button-wrapper {
+  display: flex;
+  justify-content: center;
+  margin-top: 17px;
 }
 </style>
